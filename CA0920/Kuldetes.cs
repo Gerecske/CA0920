4e848fdbcb5729adc0fb96e312774dfcb41ff753
szilagyi.gergely <szilagyi.gergely@28-09>
2022-09-20 09:27:13 +0200

    Add project files.

---
+++ CA0920/Kuldetes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA0920
+{
+    internal class Kuldetes
+    {
+        public string Kod { get; set; }
+        public DateOnly KilovesNapja { get; set; }
+        public string SikloNeve { get; set; }
+        private int KoldetesenToltotNap { get; set; }
+        private int KoldetesenToltotOra { get; set; }
+
+        public int KuldetesHossza
+        {
+            get
+            {
+                return KoldetesenToltotNap * 24 + KoldetesenToltotOra;
+            }
+        }
+        public string ErkezesHelye { get; set; }
+        public int UtasokSzama { get; set; }
+        public Kuldetes(string fileEgySora)
+        {
+            var adattagok = fileEgySora.Split(';');
+            Kod = adattagok[0];
+            KilovesNapja = DateOnly.Parse(adattagok[1]);
+            SikloNeve = adattagok[2];
+            KoldetesenToltotNap = int.Parse(adattagok[3]);
+            KoldetesenToltotOra = int.Parse(adattagok[4]);
+            ErkezesHelye = adattagok[5];
+            UtasokSzama = int.Parse(adattagok[6]);
+        }
+    }
+}
---
+++ CA0920/Program.cs
@@ -0,0 +1,10 @@
+﻿using CA0920;
+
+//beolvasas
+List<Kuldetes> kuldetesek = new();
+
+using StreamReader sr = new(@"..\..\..\res\kuldetesek.csv");
+while (!sr.EndOfStream) kuldetesek.Add(new Kuldetes(sr.ReadLine()));
+
+//f3
+Console.WriteLine($"3.feladat:\n\tÖsszesen {kuldetesek.Count} alkalommal indítottak űrhajót.");
